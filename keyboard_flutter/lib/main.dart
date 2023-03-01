a089fcdf2f7e763867dc814a33a19c03e5f71789
thieuld2000 <thieuld2000>
2023-03-01 20:15:31 +0700

    update

--- keyboard_flutter/lib/main.dart
+++ keyboard_flutter/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,108 +9,85 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Demo Lock Screen',
-      theme: ThemeData(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: const HomePage(),
-      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
 
   @override
-  // ignore: library_private_types_in_public_api
-  _HomePageState createState() => _HomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-      child: GridView.count(
-        crossAxisCount: 3,
-        mainAxisSpacing: 10,
-        crossAxisSpacing: 10,
-        shrinkWrap: true,
-        padding: EdgeInsets.only(top: 50, left: 50, right: 50),
-        children: [
-          ButtonKeyboard(
-            number: '1',
-            letters: '',
-          ),
-          ButtonKeyboard(
-            number: '2',
-            letters: 'ABC',
-          ),
-          ButtonKeyboard(
-            number: '3',
-            letters: 'DEF',
-          ),
-          ButtonKeyboard(
-            number: '4',
-            letters: 'GHI',
-          ),
-          ButtonKeyboard(
-            number: '5',
-            letters: 'JKL',
-          ),
-          ButtonKeyboard(
-            number: '6',
-            letters: 'MNO',
-          ),
-          ButtonKeyboard(
-            number: '7',
-            letters: 'PQRS',
-          ),
-          ButtonKeyboard(
-            number: '8',
-            letters: 'TUV',
-          ),
-          ButtonKeyboard(
-            number: '9',
-            letters: 'WXYZ',
-          ),
-          DeleteButton(
-            text: 'DELETE',
-          ),
-          ButtonKeyboard(
-            number: '0',
-            letters: '',
-          ),
-          DeleteAllButton(
-            text: 'DELETE ALL',
-          ),
-        ],
-      ),
-    ));
+class _MyHomePageState extends State<MyHomePage> {
+  var isInputList = [false, false, false, false];
+
+  var dotList = [
+    dotView(false),
+    dotView(false),
+    dotView(false),
+    dotView(false)
+  ];
+  String Dialnum = "";
+  void btnClicked(String btnText) {
+    Dialnum += btnText;
+    if ((Dialnum.length > 4 || Dialnum.length == 0)) {
+      Dialnum = "";
+      isInputList = [false, false, false, false];
+    } else if (Dialnum.length == 1) {
+      isInputList = [true, false, false, false];
+    } else if (Dialnum.length == 2) {
+      isInputList = [true, true, false, false];
+    } else if (Dialnum.length == 3) {
+      isInputList = [true, true, true, false];
+    } else if (Dialnum.length == 4) {
+      isInputList = [true, true, true, true];
+    }
+
+    setState(() {
+      dotList = isInputList.map((e) => dotView(e)).toList();
+    });
   }
-}
 
-class ButtonKeyboard extends StatelessWidget {
-  final String? number;
-  final String? letters;
+  void btnDelete(String btnDelete) {
+    if (btnDelete == 'DELETE') {
+      Dialnum = Dialnum.substring(0, Dialnum.length - 1);
+      if (Dialnum.length == 0) {
+        isInputList = [false, false, false, false];
+      } else if (Dialnum.length == 1) {
+        isInputList = [true, false, false, false];
+      } else if (Dialnum.length == 2) {
+        isInputList = [true, true, false, false];
+      } else if (Dialnum.length == 3) {
+        isInputList = [true, true, true, false];
+      } else if (Dialnum.length == 4) {
+        isInputList = [true, true, true, true];
+      }
+    }
 
-  ButtonKeyboard({this.number, this.letters});
+    setState(() {
+      dotList = isInputList.map((e) => dotView(e)).toList();
+    });
+  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: 50,
-        height: 50,
-        child: FloatingActionButton(
-          onPressed: () {},
+  Widget DialKey(String number, letter) {
+    return Container(
+      width: 50,
+      height: 50,
+      child: FloatingActionButton(
+          onPressed: () => btnClicked(number),
           backgroundColor: Colors.grey,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
                 '$number',
@@ -118,58 +97,109 @@ class ButtonKeyboard extends StatelessWidget {
                     fontWeight: FontWeight.w400),
               ),
               Text(
-                '$letters',
+                '$letter',
                 style: TextStyle(
                     color: Colors.white,
-                    fontSize: 8,
+                    fontSize: 10,
                     fontWeight: FontWeight.w400),
               )
             ],
-          ),
+          )),
+    );
+  }
+
+  Widget Deletebtn(String textD) {
+    return Container(
+      width: 130,
+      height: 50,
+      child: TextButton(
+        onPressed: () => btnDelete(textD),
+        child: Text('$textD'),
+      ),
+    );
+  }
+
+  Widget DeleteBtnAll(String text) {
+    return Container(
+      width: 130,
+      height: 50,
+      child: TextButton(
+        onPressed: () => btnClicked(text),
+        child: Text('$text'),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Text('Enter Passcode'),
+            ]),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [dotList[0], dotList[1], dotList[2], dotList[3]],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                DialKey('1', ''),
+                DialKey('2', 'ABC'),
+                DialKey('3', 'DEF'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                DialKey('4', 'GHI'),
+                DialKey('5', 'JKL'),
+                DialKey('6', 'MNO'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                DialKey('7', 'PQRS'),
+                DialKey('8', 'TUV'),
+                DialKey('9', 'WXYZ'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                DeleteBtnAll('DELETE ALL'),
+                DialKey('0', '+'),
+                Deletebtn('DELETE')
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
-class DeleteButton extends StatelessWidget {
-  final String? text;
-
-  DeleteButton({this.text});
-
+class dotView extends StatelessWidget {
+  dotView(this.isInput);
+  final isInput;
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Container(
-            width: 100,
-            height: 50,
-            child: TextButton(
-                onPressed: () {},
-                child: Text('$text',
-                    style: TextStyle(
-                        color: Colors.red,
-                        fontSize: 13,
-                        fontWeight: FontWeight.bold)))));
-  }
-}
-
-class DeleteAllButton extends StatelessWidget {
-  final String? text;
-
-  DeleteAllButton({this.text});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-        child: Container(
-            width: 100,
-            height: 50,
-            child: TextButton(
-                onPressed: () {},
-                child: Text('$text',
-                    style: TextStyle(
-                        color: Colors.red,
-                        fontSize: 13,
-                        fontWeight: FontWeight.bold)))));
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(7.0),
+          // color: isInput ? Colors.red : Colors.blue,
+          color: Colors.red),
+      width: 16,
+      height: 16,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(9.0)),
+            color: isInput ? Colors.red : Colors.white),
+      ),
+    );
   }
 }
